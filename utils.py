08ad9c1714ef87d18ed92248c2445a33c34c161f
simonlutz-tudortmund <126464029+simonlutz-tudortmund@users.noreply.github.com>
2023-03-01 16:48:00 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -144,11 +144,11 @@ def show_perturbations(epsilon, image):
     ax1.set_title('original')
     ax1.imshow(image,cmap='gray')
     ax2.set_title('minimal values')
-    ax2.imshow(rdm_image,cmap='gray')
+    ax2.imshow(min_image,cmap='gray')
     ax3.set_title('maximal values')
     ax3.imshow(max_image,cmap='gray')
     ax4.set_title('random values')
-    ax4.imshow(min_image,cmap='gray')
+    ax4.imshow(rdm_image,cmap='gray')
    
     ax1.axis('off')
     ax2.axis('off')
@@ -157,8 +157,5 @@ def show_perturbations(epsilon, image):
     
     plt.show()
     
-    print(image[0][0])
-    print(min_image[0][0])
-    print(max_image[0][0])
-    print(rdm_image[0][0])
+    print(max_image)
     
